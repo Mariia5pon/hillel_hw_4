e77c480a9844007aed092b35322a553d05f6ed92
Mariia5pon <mariia5pon@gmail.com>
2024-02-01 14:08:45 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,8 @@
+//Отримати від користувача 3 рядки та вивести їх у довільному порядку однією командою (конкатенація);
+let string1 = prompt("Введіть ім'я:");
+let string2 = prompt("Введіть призвіще:");
+let string3 = prompt("Введіть по-батькові:");
+
+let result = string2 + " " + string1 + " " + string3;
+
+alert(result);
